ebea3fee74ab45629222749d48c3e82db573b101
jlafrance3 <jlafranceiii@proton.me>
2025-04-16 22:12:27 -0400

    Navigation Drawer

--- app/src/main/java/edu/quinnipiac/ser210/rezippy/navigation/Navigation.kt
+++ app/src/main/java/edu/quinnipiac/ser210/rezippy/navigation/Navigation.kt
@@ -6,10 +6,16 @@ package edu.quinnipiac.ser210.rezippy.navigation
 
 import android.content.Intent
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Share
@@ -17,23 +23,33 @@ import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +62,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import edu.quinnipiac.ser210.rezippy.R
 import edu.quinnipiac.ser210.rezippy.api.RandomRecipeData.RandomRecipes
 import edu.quinnipiac.ser210.rezippy.api.RecipeAPI
 import edu.quinnipiac.ser210.rezippy.model.RecipeViewModel
@@ -63,39 +80,153 @@ fun Navigation(){
     val randomRecipesResult = recipeViewModel.randomRecipesResult.observeAsState()
     val randomRecipesResponse = randomRecipesResult.value    // Random recipes to populate HomeScreen
 
-    Scaffold (
-        topBar = {
-            NavBar(
+    // Navigation Drawer
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Column (
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                        .background(MaterialTheme.colorScheme.tertiaryContainer)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    //Drawer title
+                    Spacer(Modifier.height(12.dp))
+                    Text(
+                        text = "Rezippy",
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    HorizontalDivider()
+
+                    // Pages section
+                    Text(
+                        text = "Pages",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    NavigationDrawerItem(
+                        label = {
+                            Text(
+                                text = "Home",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                                fontWeight = FontWeight.Bold
+                            )},
+                        selected = false,
+                        onClick = {}
+                    )
+                    NavigationDrawerItem(
+                        label = {
+                            Text(
+                                text = "Bookmarked",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                                fontWeight = FontWeight.Bold
+                            )},
+                        selected = false,
+                        onClick = {}    //TODO: Bookmarked navigation
+                    )
+                    NavigationDrawerItem(
+                        label = {
+                            Text(
+                                text = "Suggestions",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                                fontWeight = FontWeight.Bold
+                            )},
+                        selected = false,
+                        onClick = {}    //TODO: AI Suggestions screen
+                    )
+
+                    // Settings
+                    Text(
+                        text = "Settings",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onTertiaryContainer,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    NavigationDrawerItem(
+                        label = {
+                            Text(
+                                text = "Settings",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                                fontWeight = FontWeight.Bold
+                            )},
+                        selected = false,
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Settings,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.onTertiaryContainer
+                            )},
+                        onClick = {}
+                    )
+                    NavigationDrawerItem(
+                        label = {
+                            Text(
+                                text = "Help",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                                fontWeight = FontWeight.Bold
+                            )},
+                        selected = false,
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Info,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.onTertiaryContainer
+                            )},
+                        onClick = {}
+                    )
+                    Spacer(Modifier.height(12.dp))
+                }
+            }
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold (
+            topBar = {
+                NavBar(
+                    navController = navController,
+                    modifier = Modifier
+                )
+            }
+        ) { innerPadding ->
+            NavHost(
                 navController = navController,
+                startDestination = Screens.HomeScreen.name,
                 modifier = Modifier
-            )
-        }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = Screens.HomeScreen.name,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            composable(Screens.HomeScreen.name) {
-                HomeScreen(
-                    randomRecipes = randomRecipesResponse?.body(),
-                    navController = navController
-                )
-            }
-            composable(
-                Screens.DetailScreen.name+"/{name}",
-                arguments = listOf(navArgument(name = "name") {type = NavType.StringType})
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                composable(Screens.HomeScreen.name) {
+                    HomeScreen(
+                        randomRecipes = randomRecipesResponse?.body(),
+                        navController = navController
+                    )
+                }
+                composable(
+                    Screens.DetailScreen.name+"/{name}",
+                    arguments = listOf(navArgument(name = "name") {type = NavType.StringType})
                 ) { backStackEntry ->
-                DetailScreen(
-                    // Pass only the clicked recipe to the DetailScreen
-                    recipe = randomRecipesResponse?.body()?.recipes?.filter { recipe ->
-                        recipe.title == backStackEntry.arguments?.getString("name")
-                    }?.firstOrNull()
-                )
+                    DetailScreen(
+                        // Pass only the clicked recipe to the DetailScreen
+                        recipe = randomRecipesResponse?.body()?.recipes?.filter { recipe ->
+                            recipe.title == backStackEntry.arguments?.getString("name")
+                        }?.firstOrNull()
+                    )
+                }
+                //TODO: Additional Screens
             }
-            //TODO: Additional Screens
         }
     }
 }
