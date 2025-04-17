46cb3c1d4da18bd5a4d6af507194539778574d15
Grant <grfood2@yahoo.com>
2025-04-16 21:32:10 -0400

    New color palette and font families. Created a new composable purely for previewing the reecipeCard.

--- app/src/main/java/edu/quinnipiac/ser210/rezippy/navigation/Navigation.kt
+++ app/src/main/java/edu/quinnipiac/ser210/rezippy/navigation/Navigation.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -118,7 +119,8 @@ fun NavBar(
             Text("Recipes",
                 color = MaterialTheme.colorScheme.onPrimary,
                 fontSize = 28.sp,
-                fontWeight = FontWeight.Bold
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
             )
         },
         navigationIcon = {
