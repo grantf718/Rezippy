264a13221657ac6b675018213c92ce2c3df70795
Grant <grfood2@yahoo.com>
2025-05-01 00:56:17 -0400

    Removed the line that was appearing below the text field

--- app/src/main/java/edu/quinnipiac/ser210/rezippy/screens/AIScreen.kt
+++ app/src/main/java/edu/quinnipiac/ser210/rezippy/screens/AIScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -119,9 +120,10 @@ fun AIScreen(){
                             unfocusedTextColor = MaterialTheme.colorScheme.onSecondary,
                             focusedTextColor = MaterialTheme.colorScheme.onSecondary,
                             focusedPlaceholderColor = MaterialTheme.colorScheme.tertiary,
-                            unfocusedPlaceholderColor = MaterialTheme.colorScheme.tertiary
+                            unfocusedPlaceholderColor = MaterialTheme.colorScheme.tertiary,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
                         ),
-
                         modifier = Modifier
                             .weight(5f)
                             .border(
